98f08521f394591920d6c743e507c84ffac472bd
minsu <jje3335@naver.com>
2024-06-28 17:23:53 +0900

    먹이주는 기능 개발

--- user.js
+++ user.js
@@ -5,3 +5,7 @@ const users = [{ name: "minsu", age: 1 }];
 function selectPet(name) {
   return pets.find(({ name: _name }) => name === _name);
 }
+
+function feedPet(pet) {
+  return `${pet.name}에게 밥을 줬습니다.`;
+}
